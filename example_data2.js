8a6cded45d273d6fdd918aaf993373a9e3689803
Logan Esenther <le@berkeley.edu>
2014-10-15 12:08:40 -0700

    Added examples

---
+++ example_data2.js
@@ -0,0 +1,74 @@
+{
+  "plot_type":4,
+  "axes_labels":["Aa","Bb","Cc","Dd"],
+  "plots":[{
+    "caption":"Plot 1",
+    "points":[
+      [0.1,0.2,200],
+      [0.2,0.3,233],
+      [0.3,0.4,266],
+      [0.4,0.5,300]
+    ]
+  },{
+    "caption":"Plot 2",
+    "points":[
+      [0.1,0.2,325],
+      [0.2,0.3,350],
+      [0.3,0.4,375],
+      [0.4,0.5,400]
+    ]
+  },{
+    "caption":"Plot 3",
+    "points":[
+      [0,0,200],
+      [0,1,300],
+      [1,0,400],
+      [1,1,500]
+    ]
+  },{
+    "caption":"Plot 4",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 5",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 6",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 7",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 8",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 9",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 10",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 11",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  },{
+    "caption":"Plot 12",
+    "points":[
+      [0.5,0.5,null]
+    ]
+  }]
+}
